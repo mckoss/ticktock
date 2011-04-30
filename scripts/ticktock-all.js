fba960e20942904b7fcd500ad88a2c38a4ee5675
Bobby Seidensticker <bseidensticker@gmail.com>
2011-04-30 12:56:08 -0700

    added 1kb grid, updated index

--- scripts/ticktock-all.js
+++ scripts/ticktock-all.js
@@ -395,52 +395,57 @@ function strip(s) {
 });
 /* Source: scripts/main.js */
 namespace.module('com.ticktocktask.main', function (exports, require) {
-var clientLib = require('com.pageforest.client');
-var dom = require('org.startpad.dom');
 
-exports.extend({
-    'onReady': onReady,
-    'getDoc': getDoc,
-    'setDoc': setDoc
+namespace.lookup('com.ticktocktask.main').defineOnce(function (ns) {
+
+    var clientLib = require('com.pageforest.client');
+    var dom = require('org.startpad.dom');
+
+    exports.extend({
+        'onReady': onReady,
+        'getDoc': getDoc,
+        'setDoc': setDoc
+    });
+
+    var client;
+    var doc;                            // Bound elements here
+
+    function onReady() {
+        handleAppCache();
+        doc = dom.bindIDs();
+        client = new clientLib.Client(exports);
+        client.saveInterval = 0;
+
+        client.addAppBar();
+    }
+
+    function setDoc(json) {
+    }
+
+    function getDoc() {
+        return {
+            blob: {
+                version: 1,
+                tasks: [],
+            },
+            readers: ['public']
+        };
+    }
+
+    // For offline - capable applications
+    function handleAppCache() {
+        if (typeof applicationCache == 'undefined') {
+            return;
+        }
+
+        if (applicationCache.status == applicationCache.UPDATEREADY) {
+            applicationCache.swapCache();
+            location.reload();
+            return;
+        }
+
+        applicationCache.addEventListener('updateready', handleAppCache, false);
+    }
+
 });
-
-var client;
-var doc;                            // Bound elements here
-
-function onReady() {
-    handleAppCache();
-    doc = dom.bindIDs();
-    client = new clientLib.Client(exports);
-    client.saveInterval = 0;
-
-    client.addAppBar();
-}
-
-function setDoc(json) {
-}
-
-function getDoc() {
-    return {
-        blob: {
-            version: 1,
-            tasks: [],
-        },
-        readers: ['public']
-    };
-}
-
-// For offline - capable applications
-function handleAppCache() {
-    if (typeof applicationCache == 'undefined') {
-        return;
-    }
-
-    if (applicationCache.status == applicationCache.UPDATEREADY) {
-        applicationCache.swapCache();
-        location.reload();
-        return;
-    }
-
-    applicationCache.addEventListener('updateready', handleAppCache, false);
-}
 });
