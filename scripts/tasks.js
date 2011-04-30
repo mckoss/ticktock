54e70cccfa300ca26dae86eb5549fef531350483
Mike Koss <mckoss@startpad.org>
2011-04-30 16:14:05 -0700

    change

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -1,4 +1,4 @@
-var clientLib = require('com.pageforest.client');
+var cLientLib = require('com.pageforest.client');
 var dom = require('org.startpad.dom');
 var types = require('org.startpad.types');
 require('org.startpad.funcs').patch();
@@ -10,11 +10,19 @@ exports.extend({
 
 function Project(options) {
     types.extend(this, options);
-    this.tasks = [];
+    if (this.tasks == undefined) {
+        this.tasks = [];
+    }
+
+    for (var i = 0; i < this.tasks.length; i++) {
+        var task = this.tasks[i];
+        this.tasks[i] = new Task(task);
+    }
 }
 
 Project.methods({
    addTask: function(task) {
+       task = new Task(task);
        this.tasks.push(task);
        return task;
    },
@@ -22,4 +30,15 @@ Project.methods({
    toJSON: function () {
        return this;
    }
+});
+
+function Task(options) {
+    types.extend(this, options);
+}
+
+Task.methods({
+   change: function (options) {
+       types.extend(this, options);
+       return this;
+   } 
 });
