4bc2ef1628123ae488926b7319b1d370dac407e7
Mike Koss <mckoss@startpad.org>
2011-05-01 07:28:56 -0700

    node.js tests

--- test/test-runner-node.js
+++ test/test-runner-node.js
@@ -2,5 +2,6 @@
 global.namespace = require('../scripts/namespace.js').namespace;
 
 require('../scripts/pandatask-all.js');
+require('../scripts/format-extra.js');
 require('./qunit-node.js');
 require('./test-tasks.js');
