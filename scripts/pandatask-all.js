c1f72c0cd7a574908788397dc32e71aba3ff9cef
Sam Grossberg <sam.grossberg@gmail.com>
2011-05-01 14:26:09 -0700

    checkbox bug

--- scripts/pandatask-all.js
+++ scripts/pandatask-all.js
@@ -794,10 +794,10 @@ var editedText;
 var editedStatus;
 
 var TASK = '<div id="{id}" class="task {className}">' +
-           '<div class="content if-not-edit">{content}' +
            '<div id="action_{id}" class="action"><input type="checkbox" /></div>' +
            '<div id="promote_{id}" class="promote icon"></div>' +
            '<div class="delete icon" id="delete_{id}"></div>' +
+           '<div class="content if-not-edit">{content}' +
            '</div>' +
            '<textarea class="if-edit"></textarea>' +
            '</div>';
