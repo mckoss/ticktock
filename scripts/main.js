08338278a4db21a8f9782b5041257e1daf18e75f
Bobby Seidensticker <bseidensticker@gmail.com>
2011-04-30 19:00:28 -0700

    saving and loading

--- scripts/main.js
+++ scripts/main.js
@@ -16,54 +16,66 @@ var project;
 var addList = [];
 
 var ADD_TASK = '<div id=add{i} class=newTask>' +
-               '<div> What:<input id=desc{i} class=desc /></div>' +
+               '<div> What:<input id=description{i} class=desc /></div>' +
                '<div> Time:<input type=number id=time{i} class=time />hours<input id=ok{i} type=button value=OK />' + 
                '</div>';
-var TASK = '<div id={id} class=task><input class=left value=l />{desc}. {time} hours<input class=right value=r /></div>';
+var TASK = '<div id={id} class=task>' +
+           '<img src="images/left.png" class=left >' +
+           '<div class=content>{description}. {time} {units}</div>' +
+           '<img src="images/right.png" class=right ></div>';
 
 function onReady() {
     handleAppCache();
     doc = dom.bindIDs();
-    
+
     project = new taskLib.Project({title: ''});
     client = new clientLib.Client(exports);
     client.saveInterval = 0;
 
-    $('#r-add').click(onAdd.curry('r'));
+    /*$('#r-add').click(onAdd.curry('r'));
     $('#w-add').click(onAdd.curry('w'));
-    $('#d-add').click(onAdd.curry('d'));
+    $('#d-add').click(onAdd.curry('d'));*/
+
+    $('.header').click(refresh);
     
-    //task.change();
-    console.log('project.tasks');
-    console.log(project.tasks)
-    
-    
-    console.log('onOk exists');
     client.addAppBar();
 }
 
 //list is r, w, d for ready working done
 function onAdd(list) {
-    console.log('list: ' + list);
     var i = addList.length;
     addList[addList.length] = list;
-    $('div.'+list+'-tasks').append(ADD_TASK.format({i: i}));
+    $('div.' + list + '-tasks').append(ADD_TASK.format({i: i}));
     $('#ok' + i).click(onOk.curry(i));
 }
 
 function onOk(i) {
   //t[i] = project.addTask({description: "task number " + i, estimated: 0, completed: 0});
-    var desc = $('#desc' + i).val();
-    var time = $('#time' + i).val();
+    var d = $('#description' + i).val();
+    var t = $('#time' + i).val();
     var list = addList[i];
-    var task = project.addTask({desc: desc, time: time, list: list});
+    var units;
+    t == 1 ? units = 'hour' : units = 'hours';
+    var task = project.addTask({description: d, time: t, list: list, units: units});
     addList[i] = undefined;
     $('#add' + i).remove();
-    $('.' + list + '-tasks').append(NEW_TASK.format({id: task.id, task.desc, task.time}));
+    $('div.' + list + '-tasks').append(TASK.format({id: task.id, description: task.description, time: task.time, units: task.units}));
+    $('div#' + task.id).find()
+}
+
+function refresh() {
+    $('.r-tasks').empty();
+    $('.w-tasks').empty();
+    $('.d-tasks').empty();
+    for (var i = 0; i < project.tasks.length; i++) {
+        var task = project.tasks[i];
+        $('div.' + task.list + '-tasks').append(TASK.format({id: task.id, description: task.description, time: task.time, units: task.units}));
+    }
 }
 
 function setDoc(json) {
     project = new taskLib.Project(json.blob);
+    refresh();
     //update UI
 }
 
